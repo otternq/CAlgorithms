f8c347e8ba3ae4f1d9a7f7571a588966ad57d3d0
otternq <otternq@gmail.com>
2013-05-11 10:36:12 -0700

    adding cunit to project

--- tests/test_insertionSort.c
+++ tests/test_insertionSort.c
@@ -4,7 +4,9 @@
 #include "../src/libs/dynarray.h"
 #include "../src/sort/insertionSort.h"
 
-int main() {
+#include "CUnit/Basic.h"
+
+void testInsertionSortWork() {
 
     dyn_array * zeroToNine = initDynArray(10);
 
@@ -19,13 +21,76 @@ int main() {
     insertionSort(zeroToNine);
 
     for (i = 0; i < 10; i++) {
-        if (zeroToNine->array[i] != afterSorted[i]) {
-            printf("%s\n", "Test Failed");
-            return EXIT_FAILURE;
-        }
+        CU_ASSERT(zeroToNine->array[i] == afterSorted[i]);
     }
 
     printf("%s\n", "Test Succeded");
-    return EXIT_SUCCESS;
 
+}
+
+/* Pointer to the file used by the tests. */
+static FILE* temp_file = NULL;
+
+/* The suite initialization function.
+ * Opens the temporary file used by the tests.
+ * Returns zero on success, non-zero otherwise.
+ */
+int init_suite1(void)
+{
+   if (NULL == (temp_file = fopen("temp.txt", "w+"))) {
+      return -1;
+   }
+   else {
+      return 0;
+   }
+}
+
+/* The suite cleanup function.
+ * Closes the temporary file used by the tests.
+ * Returns zero on success, non-zero otherwise.
+ */
+int clean_suite1(void)
+{
+   if (0 != fclose(temp_file)) {
+      return -1;
+   }
+   else {
+      temp_file = NULL;
+      return 0;
+   }
+}
+
+
+/* The main() function for setting up and running the tests.
+ * Returns a CUE_SUCCESS on successful running, another
+ * CUnit error code on failure.
+ */
+int main()
+{
+   CU_pSuite pSuite = NULL;
+
+   /* initialize the CUnit test registry */
+   if (CUE_SUCCESS != CU_initialize_registry())
+      return CU_get_error();
+
+   /* add a suite to the registry */
+   pSuite = CU_add_suite("Suite_1", init_suite1, clean_suite1);
+   if (NULL == pSuite) {
+      CU_cleanup_registry();
+      return CU_get_error();
+   }
+
+   /* add the tests to the suite */
+   /* NOTE - ORDER IS IMPORTANT - MUST TEST fread() AFTER fprintf() */
+   if ((NULL == CU_add_test(pSuite, "test that insertion sort is working", testInsertionSortWork)) )
+   {
+      CU_cleanup_registry();
+      return CU_get_error();
+   }
+
+   /* Run all tests using the CUnit Basic interface */
+   CU_basic_set_mode(CU_BRM_VERBOSE);
+   CU_basic_run_tests();
+   CU_cleanup_registry();
+   return CU_get_error();
 }
